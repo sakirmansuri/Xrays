b07f73b935f65c62c1975fbb64e7e30f93466182
SAKIR MANSURI <sakir.01am@gmail.com>
2026-04-29 13:19:00 +0530

    Comment out get_model function in model.py
    Comment out the get_model function and its contents.

--- src/model.py
+++ src/model.py
@@ -3,15 +3,23 @@ import torch.nn as nn
 import torchvision.models as models
 
 def get_model(num_classes=2):
+    import torchvision.models as models
+    import torch.nn as nn
 
-    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-
-    # Freeze base layers
-    for param in model.parameters():
-        param.requires_grad = False
-
-    # Replace final layer
-    in_features = model.fc.in_features
-    model.fc = nn.Linear(in_features, num_classes)
-
+    model = models.resnet18(weights=None)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
+
+# def get_model(num_classes=2):
+
+#     model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
+#     # Freeze base layers
+#     for param in model.parameters():
+#         param.requires_grad = False
+
+#     # Replace final layer
+#     in_features = model.fc.in_features
+#     model.fc = nn.Linear(in_features, num_classes)
+
+#     return model
